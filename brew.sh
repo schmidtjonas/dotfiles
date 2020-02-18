1c55de3b4ebf736329ad37e8cf5545cccc4c51fc
Jonas Schmidt <mail@schmidt-jonas.de>
2020-02-19 00:41:37 +0100

    small changes

--- brew.sh
+++ brew.sh
@@ -4,6 +4,8 @@ brew upgrade
 
 brew doctor
 
+echo "Installing all programs I just remembered to install..."
+
 brew install python
 brew install tree
 brew install vim
@@ -11,14 +13,13 @@ brew install neovim
 brew install zsh
 brew install oh-my-zsh
 
-
 brew cask install visual-studio-code
 brew cask install iterm2
 brew cask install jetbrains-toolbox
 brew cask install macpass
 brew cask install onedrive
 brew cask install scroll-reverser
-brew cask install sublime-text
-brew cask install texstudio
 brew cask install typora
 brew cask install telegram
+brew cask install google-chrome
+brew cask install karabiner-elements
